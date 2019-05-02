a1eb3eadffdf96a69c5960cd5910dd04e5fb4f50
Titus Pinta <titus.pinta@gmail.com>
2019-05-02 17:04:35 +0300

    Final Version

--- optim/ssa1.py
+++ optim/ssa1.py
@@ -15,7 +15,7 @@ class SSA1(Optimizer):
 
         for group in self.param_groups:
             for p in group['params']:
-                self.state[p]['u'] = p.data
+                self.state[p]['theta'] = p.data
                 self.state[p]['v'] = torch.zeros_like(p.data)
 
 
@@ -24,7 +24,7 @@ class SSA1(Optimizer):
 
     def step(self, closure):
         self.state['n_iter'] += 1
-        beta = self.state['n_iter'] / (self.state['n_iter'] + 3)
+        beta = self.state['n_iter'] / (self.state['n_iter'] + 3) #updte inertial parameter
 
         for group in self.param_groups:
             lr = group['lr']
@@ -32,7 +32,7 @@ class SSA1(Optimizer):
                 if p.grad is None:
                     continue
                 state = self.state[p]
-                p.data = state['u'].add(state['v'].mul(lr * beta))
+                p.data = state['theta'] + lr * beta * state['v'] #compute value under gradient
 
         loss = closure()
         for group in self.param_groups:
@@ -43,7 +43,7 @@ class SSA1(Optimizer):
                     continue
                 state = self.state[p]
                 d_p = p.grad.data
-                state['v'] = state['v'].mul(1 - lr * beta).sub(d_p.mul(lr)).mul(beta ** k)
-                state['u'].add_(p.data.sub(state['u']).mul((1 - lr * beta) * beta).sub(d_p.mul(lr ** 2)))
+                state['theta'] = state['theta'] + lr*(1 - lr*beta) * state['v'] - lr ** 2 * d_p #update information
+                state['v'] = beta ** k *((1 - lr * beta) * state['v'] - lr * d_p) #update velocity
 
         return loss
--- optim/ssa2.py
+++ optim/ssa2.py
@@ -14,7 +14,7 @@ class SSA2(Optimizer):
 
         for group in self.param_groups:
             for p in group['params']:
-                self.state[p]['u'] = p.data
+                self.state[p]['theta'] = p.data
                 self.state[p]['v'] = torch.zeros_like(p.data)
 
 
@@ -23,7 +23,7 @@ class SSA2(Optimizer):
 
     def step(self, closure):
         self.state['n_iter'] += 1
-        beta = self.state['n_iter'] / (self.state['n_iter'] + 3)
+        beta = self.state['n_iter'] / (self.state['n_iter'] + 3) #update inertial parameter
 
         for group in self.param_groups:
             lr = group['lr']
@@ -31,7 +31,7 @@ class SSA2(Optimizer):
                 if p.grad is None:
                     continue
                 state = self.state[p]
-                p.data = state['u'].add(state['v'].mul(lr * beta))
+                p.data = state['theta'] + lr * beta * state['v'] #compute value under gradient
 
         loss = closure()
         for group in self.param_groups:
@@ -43,7 +43,8 @@ class SSA2(Optimizer):
                     continue
                 state = self.state[p]
                 d_p = p.grad.data
-                state['v'] = state['v'].mul((1 - lr * beta) ** q).sub(d_p.mul(lr * beta ** k))
-                state['u'].add_(p.data.sub(state['u']).mul((1 - lr * beta) / beta).sub(d_p.mul(lr ** 2)))
+                state['theta'] = state['theta'] + lr * (1 - lr * beta) * state['v'] #update information
+                state['v'] = beta ** k *((1 - lr * beta) ** q * state['v'] - lr * d_p) #update velocity
+
 
         return loss
