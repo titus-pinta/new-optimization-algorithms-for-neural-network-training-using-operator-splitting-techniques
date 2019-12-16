cb5fe5ff3757c6ba21fa8487e78f4ac066bf08f3
Titus Pinta <titus.pinta@gmail.com>
2019-12-16 23:14:32 +0000

    Test for IMDB

--- test.py
+++ test.py
@@ -1,21 +1,39 @@
 import torch
 import torch.nn.functional as F
 
+
 def test(args, model, device, test_loader, result_correct, result_loss, scatter):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
+        if args.imdb:
+            for batch in test_loader:
+                text, text_lengths = batch.text
+                predictions = model(text, text_lengths).squeeze(1)
+                loss = loss_function(predictions, batch.label)
 
-            if scatter:
-                target.scatter_(10)
+                def correct_preds(preds, y):
+                    rounded_preds = torch.round(torch.sigmoid(preds))
+                    correct = (rounded_preds == y).float()
+                    acc = correct.sum()
+                    return acc
+                test_loss += loss.item()
+                correct += correct_preds(predictions, batch.label)
 
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+        else:
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+
+                if scatter:
+                    target.scatter_(10)
+
+                # sum up batch loss
+                test_loss += F.nll_loss(output, target, reduction='sum').item()
+                # get the index of the max log-probability
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
@@ -25,4 +43,3 @@ def test(args, model, device, test_loader, result_correct, result_loss, scatter)
 
     result_correct.append(correct / len(test_loader.dataset))
     result_loss.append(test_loss)
-
